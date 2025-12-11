6103d3c0f1b90982e932c6d2ac49c2243fe9364d
Bartu Özaşçı <bartuozasci@gmail.com>
2025-12-11 10:44:04 +0300

    Add files via upload

---
+++ src/data/messages.js
@@ -0,0 +1,35 @@
+// Yılbaşı mesajları veritabanı
+export const messages = [
+  "Bugün advent calendarımızın ilk günü ilk güne de seni çok sevdiğimi söyleyerek başlamak istedim ❤️", // 1 Aralık
+  "Bugün benim doğum günüm eminim ki çok güzel sürprizler hazırladın ama en güzeli sensin 🍰", // 2 Aralık
+  "İyi ki varsın  ❤️", // 3 Aralık
+  "Kendine güzel bir takipçi manifesti yap inan bana gerçek olucak 777 🎁", // 4 Aralık
+  "Bugün çok güzel görünüyorsun", // 5 Aralık
+  "Geçen sene bugün napıyordun ? ✅", // 6 Aralık
+  "Eski yılbaşı fotoğraflarına bak, anıları tazele 📷", // 7 Aralık
+  "Eroscuğa bir öpücük ver 💋", // 8 Aralık
+  "Sevdiğin bir film izle, rahatlat kendini 🎬", // 9 Aralık
+  "Sana bugün için bir çikolata hediyem var 🍫", // 10 Aralık
+  "Bugün için Bartuştan 10 tl hediye kazandın! 🎉", // 11 Aralık
+  "Bugün paylaşıcağın reels çok izlenicek buna inan ! 🙎‍♀️ ", // 12 Aralık
+  "Akşam hamburger yemeye ne dersin ? 🍔", // 13 Aralık
+  "Yılbaşı ağacını süslemeye başlayalım! 🎄", // 14 Aralık
+  "Bugün Bartu'nun senin için minik bir süprizi var 🎁", // 15 Aralık
+  "Bugün senin günün Bartuştan git ve öpücüğünü istemeyi unutma 🤗", // 16 Aralık
+  "1 tl hediye kazandın! 🎉", // 17 Aralık
+  "Özel kurabiye ödülünü iste 🍪 ", // 18 Aralık
+  "Spotifyda playlistimizi aç ve dinle !", // 19 Aralık
+  "Bartu senden güzel bir mesaj bekliyor sevgini göstermeyi unutma ❤️", // 20 Aralık
+  "Ev yapımı lezzetler hazırla antep fıstıklı brownie olabilir ? 🍰", // 21 Aralık
+  "Yılın en uzun gecesi geride kaldı artık yaz geliyor yaz alışverişi mi yoksa ? 🛒", // 22 Aralık
+  "Bartu'nun sana bugün için 5 tl hediyesi var 🎁", // 23 Aralık
+  "Yarın resmi olarak noel başlıyor ve noel pazarları hareketleniyor 👀", // 24 Aralık
+  "Mutlu Noeller! Sevgilinle sevgi dolu bir gün geçir! 🎄🎅", // 25 Aralık
+  "Bartu senin için özel bir çikolata aldı git ve iste 🍫", // 26 Aralık
+  "Bugün çok güzel görünüyorsun 💅", // 27 Aralık
+  "Bugün seni tatlı yemeye götürüyorum 🍻", // 28 Aralık
+  "Yarın Gürcistana yolculuğumuz başlıyor kemerleri bağla 🧳 ", // 29 Aralık
+  "Gürcistan yolları bekler bavul hazırsa beraber ilk yurtdışı maceramız 🌍", // 30 Aralık
+  "Yılın son günü, ve seninle dolu geride kalan bir yıl! 🥂", // 31 Aralık
+  "Mutlu Yıllar! Seni çok seviyorum bu yılda da seni çok sevicem 🎊🎉❤️", // 1 Ocak
+];
